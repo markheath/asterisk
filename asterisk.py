890ceda57ee2af1ef78fb3957ab890f305e47a39
Mark Heath <147668+markheath@users.noreply.github.com>
2010-10-08 18:43:01 +0000

    some refactoring out into a GameArea class

--- asterisk.py
+++ asterisk.py
@@ -2,6 +2,7 @@
 # by Mark Heath, http://mark-dot-net.blogspot.com
 import mvvm
 import clr
+import os
 clr.AddReference("WindowsBase")
 clr.AddReference("PresentationCore")
 clr.AddReference("PresentationFramework")
@@ -16,22 +17,19 @@ from System.Windows.Input import Key
 class ViewModel(mvvm.ViewModelBase):
     def __init__(self, xaml):
         mvvm.ViewModelBase.__init__(self)
+        self.highScore = HighScore()
         self.Level = "Level 1"
-        self.Record = "Level 1 by nobody"
-        self.canvas = xaml.gameCanvas
+        self.Record = self.highScore.Message
         self.NewGameCommand = mvvm.Command(self.newGame)
         self.setupGameLoop()
         self.currentLevel = 0
         self.xPosition = 0
         self.yPosition = 0
         self.keyDown = False
-        self.gameArea = (300,200)
-        self.redrawScreen()
-        self.rand = Random()
+        self.gameArea = GameArea(xaml.gameCanvas)
         xaml.Root.KeyDown += self.onKeyDown
         xaml.Root.KeyUp += self.onKeyUp
         xaml.Root.Closing += lambda sender, args: self.Timer.Stop()
-        self.stars = []
 
     def setupGameLoop(self):
         self.Timer = DispatcherTimer()
@@ -41,27 +39,11 @@ class ViewModel(mvvm.ViewModelBase):
     def onTick(self, sender, args):
         self.xPosition += 1
         self.yPosition += (-1 if self.keyDown else 1)
+        
         self.checkPosition()
-        self.polyline.Points.Add(Point(self.xPosition, self.yPosition))
-
-    def isCollision(self):
-        width = self.gameArea[0]
-        height = self.gameArea[1]
-        if self.yPosition <= 0 or self.yPosition >= height:
-            return True
-        if self.xPosition >= width:
-            return not ((height / 2 + 15) > self.yPosition > (height / 2 - 15))
-        for star in self.stars:
-            testX = self.xPosition - Canvas.GetLeft(star)
-            testY = self.yPosition - Canvas.GetTop(star)
-            if mvvm.CheckCollisionPoint(Point(testX, testY), star):
-                return True
 
     def checkPosition(self):
-        crash = False
-
-        if self.isCollision():
-            crash = True
+        crash = not self.gameArea.AddNewPosition(self.xPosition, self.yPosition)
 
         if crash:
             self.Timer.Stop()
@@ -77,7 +59,7 @@ class ViewModel(mvvm.ViewModelBase):
             self.NewGameCommand.canExecute = True
             self.NewGameCommand.RaiseCanExecuteChanged()
         else:
-            if self.xPosition >= self.gameArea[0]:
+            if self.xPosition >= self.gameArea.Width:
                 self.newLevel()
 
     def newGame(self):
@@ -90,13 +72,29 @@ class ViewModel(mvvm.ViewModelBase):
     def newLevel(self):
         self.currentLevel += 1
         self.xPosition = 0
-        self.yPosition = self.gameArea[1] / 2
+        self.yPosition = self.gameArea.Height / 2
         self.Level = 'Level ' + str(self.currentLevel)
         self.RaisePropertyChanged("Level")
         self.keyDown = False
-        self.redrawScreen();
-        self.polyline.Points.Clear()
-        self.polyline.Points.Add(Point(self.xPosition, self.yPosition))
+        self.gameArea.RedrawScreen(self.currentLevel);
+        self.gameArea.AddNewPosition(self.xPosition, self.yPosition)
+
+    def onKeyDown(self, sender, args):
+        if (args.Key == Key.Space):
+            self.keyDown = True
+
+    def onKeyUp(self, sender, args):
+        if (args.Key == Key.Space):
+            self.keyDown = False
+
+class GameArea:
+    Height = 200
+    Width = 300
+    
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.rand = Random()
+        self.RedrawScreen(0)
     
     def addLine(self, _from, to):
         line = Line()
@@ -107,37 +105,68 @@ class ViewModel(mvvm.ViewModelBase):
         line.Stroke = Brushes.White
         line.StrokeThickness = 2.0
         self.canvas.Children.Add(line)
-        
-    def redrawScreen(self):
+    
+    def isCollision(self, x, y):
+        width = self.Width
+        height = self.Height
+        if y <= 0 or y >= height:
+            return True
+        if x >= width:
+            return not ((height / 2 + 15) > y > (height / 2 - 15))
+        for star in self.stars:
+            testX = x - Canvas.GetLeft(star)
+            testY = y - Canvas.GetTop(star)
+            if mvvm.CheckCollisionPoint(Point(testX, testY), star):
+                return True
+    
+    def AddNewPosition(self, x, y):
+        if self.isCollision(x, y):
+            return False
+        self.polyline.Points.Add(Point(x, y))
+        return True
+    
+    def RedrawScreen(self, level):
         self.canvas.Children.Clear()
-        width = self.gameArea[0]
-        height = self.gameArea[1]
+        width = self.Width
+        height = self.Height
         self.addLine((0,0), (width,0)) #line across top
         self.addLine((0,height), (width, height)) # line across botom
         self.addLine((width,0), (width, height / 2 - 15))
         self.addLine((width,height / 2 + 15), (width, height))
 
         self.stars = []
-        for n in range(self.currentLevel * 3):
+        for n in range(level * 3):
             star = mvvm.XamlLoader('star.xaml').Root
             self.stars.append(star)
-            Canvas.SetLeft(star, self.rand.Next(10, self.gameArea[0] - 10))
-            Canvas.SetTop(star, self.rand.Next(0, self.gameArea[1] - 10))
+            Canvas.SetLeft(star, self.rand.Next(10, self.Width - 10))
+            Canvas.SetTop(star, self.rand.Next(2, self.Height - 10))
             self.canvas.Children.Add(star)
             
-        #   g.DrawString("*",fnt,new SolidBrush(Color.White),new Point(rand.Next(10, rectMain.Width - 10),rand.Next(0, rectMain.Height - 10)));
         self.polyline = Polyline()
         self.polyline.Stroke = Brushes.Yellow
         self.polyline.StrokeThickness = 2.0
         self.canvas.Children.Add(self.polyline)
 
-    def onKeyDown(self, sender, args):
-        if (args.Key == Key.Space):
-            self.keyDown = True
+class HighScore:
+    def __init__(self):
+        self.Level = 0
+        self.Person = 'Nobody'
+        self.LoadHighScore()
 
-    def onKeyUp(self, sender, args):
-        if (args.Key == Key.Space):
-            self.keyDown = False
+    def getMessage(self):
+        return 'Level {0} by {1}'.format(self.Level, self.Person)
+
+    Message = property(getMessage)
+
+    def SaveHighScore(self, score, person):
+        f = open('record.txt', 'w')
+        f.write('{0}:{1}'.format(score,person))
+
+    def LoadHighScore(self):
+        if os.path.isfile('record.txt'):
+            record = open('record.txt').readline()
+            parts = record.split(':')
+
 
 xaml = mvvm.XamlLoader('asterisk.xaml')
 xaml.Root.DataContext = ViewModel(xaml)
